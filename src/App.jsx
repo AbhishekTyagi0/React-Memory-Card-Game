ad9c483021f788f5c264defb4315c287bde5942b
AbhishekTyagi0 <abhishektyagigi4310@gmail.com>
2023-11-29 23:23:24 +0530

    app setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='appContainer'>
+      <h1>Memory Card Game</h1>
+      <div className='container'></div>
+    </div>
+  )
+}
+
+export default App;
